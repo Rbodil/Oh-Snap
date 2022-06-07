3e281867d0cea03a2d1801c8dfbf15e6439fbb7a
Reagan Bodily <reaganbodily@gmail.com>
2022-06-07 15:20:56 -0600

    built modal

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Nav from "./components/Nav";
 import About from "./components/About";
 import Gallery from "./components/Gallery";
-import PhotoList from "./components/PhotoList";
 import ContactForm from "./components/Contact";
 
 import './App.css';
@@ -19,20 +18,26 @@ function App() {
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
       <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}
+         categories={categories}
+         setCurrentCategory={setCurrentCategory}
+         currentCategory={currentCategory}
+         contactSelected={contactSelected}
+         setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <div>
-          <ContactForm></ContactForm>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
-        </div>
+          {!contactSelected ? (
+            <>
+              <Gallery currentCategory={currentCategory}></Gallery>
+              <About></About>
+            </>
+          ) : (
+            <ContactForm></ContactForm>
+          )}
       </main>
     </div>
   );
---
+++ src/components/Contact/__tests__/index.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import ContactForm from '..'
+
+afterEach(cleanup);
+
+describe('ContactForm is rendering', () => {
+    it('renders', () => {
+        render(<ContactForm />);
+    });
+
+    it('renders', () => {
+        const { asFragment } = render(<ContactForm />)
+        expect(asFragment()).toMatchSnapshot()
+    });
+});
