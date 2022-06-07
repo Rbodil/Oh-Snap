d6922645688ac91b7321564b706ce9170830c5c6
Reagan Bodily <reaganbodily@gmail.com>
2022-06-06 22:42:27 -0600

    components and tests

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import About from '../index';
+import About from '..';
 
 afterEach(cleanup);
 
-describe('About component', () => {
-    // First Test
+describe('About component renders', () => {
+
     it('renders', () => {
         render(<About />);
     });
 
-    // Second Test
     it('matches snapshot DOM node structure', () => {
         const { asFragment } = render(<About />);
         expect(asFragment()).toMatchSnapshot();
